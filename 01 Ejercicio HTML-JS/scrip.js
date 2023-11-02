4968a29168c5ca5086aa0d5d5b267b3fd568d284
b.segovia.iccp@gmail.com <b.segovia.iccp@gmail.com>
2023-11-02 11:31:44 -0300

    ok

---
+++ 01 Ejercicio HTML-JS/scrip.js
@@ -0,0 +1,53 @@
+let edad = 18;
+
+if (edad >= 18) {
+    console.log('ústed es mayor de edad');
+}
+else {
+    console.log('usted no es mayor de edad');
+}
+
+
+let temperatura = 20;
+
+if (temperatura < 0) {
+    console.log("Hace frio");
+} else if (temperatura >= 0 && temperatura < 25) {
+    console.log("La temperatura es agradable");
+} else 
+    console.log("Hace calor");
+
+
+
+  let nombreUsuario = "usuario123";
+  let contraseña = "secreto";
+
+  if (nombreUsuario === "usuario123" && contraseña === "secreto") {
+    console.log("Acceso concedido");
+  } else {
+    console.log("Acceso denegaddo");
+  }
+
+  let numero = 2;
+  
+    if (numero > 0 ) {
+      console.log("número es positivo"); 
+     } else if (numero < 0) {
+      console.log("El numero es negativo");
+     } else {
+        console.log("El numero es igual a cero");
+     }
+
+
+     let puntacion = 90;
+
+     if (puntacion >= 90) {
+        console.log("Excelente");
+     } else if (puntacion >= 70) {
+        console.log('Buen trabajo') 
+     } else {
+        console.log('Necesitas mejorar')
+     }
+
+     git config --global user.name "b.segovia.iccp@gmail.com"
+     git config user.name "Benjaminsegovi"
